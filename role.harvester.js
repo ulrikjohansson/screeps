cf3f85fd883b382ee484753a299eb2c891714bc3
Ulrik <ulrik.johansson@gmail.com>
2017-11-19 15:21:05 +0100

    Add function for using pathfinder

--- role.harvester.js
+++ role.harvester.js
@@ -81,6 +81,59 @@ var roleHarvester = {
         }
 
 	},
+    findSourcePath: function(creep) {
+		let goals = _.map(creep.room.find(FIND_SOURCES), function(source) {
+			// We can't actually walk on sources-- set `range` to 1 
+			// so we path next to it.
+			return { pos: source.pos, range: 1 };
+		  });
+		  console.log("Goals");
+		  console.log(JSON.stringify(goals, null, 2));
+
+		  let ret = PathFinder.search(
+			creep.pos, goals,
+			{
+			  // We need to set the defaults costs higher so that we
+			  // can set the road cost lower in `roomCallback`
+			  plainCost: 2,
+			  swampCost: 10,
+
+			  roomCallback: function(roomName) {
+
+				let room = Game.rooms[roomName];
+				console.log("FindSource room: " + JSON.stringify(room, null, 2));
+				// In this example `room` will always exist, but since 
+				// PathFinder supports searches which span multiple rooms 
+				// you should be careful!
+				if (!room) return;
+				let costs = new PathFinder.CostMatrix;
+
+				room.find(FIND_STRUCTURES).forEach(function(struct) {
+				  if (struct.structureType === STRUCTURE_ROAD) {
+					// Favor roads over plain tiles
+					costs.set(struct.pos.x, struct.pos.y, 1);
+				  } else if (struct.structureType !== STRUCTURE_CONTAINER &&
+							 (struct.structureType !== STRUCTURE_RAMPART ||
+							  !struct.my)) {
+					// Can't walk through non-walkable buildings
+					costs.set(struct.pos.x, struct.pos.y, 0xff);
+				  }
+				});
+
+				// Avoid creeps in the room
+				room.find(FIND_CREEPS).forEach(function(creep) {
+				  costs.set(creep.pos.x, creep.pos.y, 0xff);
+				});
+
+                console.log(JSON.stringify(costs, null, 2));
+				return costs;
+			  },
+			}
+		  );
+
+          console.log("Path result:\n" + JSON.stringify(ret, null, 2));
+		  return ret.path;
+    },
 	runHarvest: function(creep) {
         target = Game.getObjectById(creep.memory.target_id);
 
