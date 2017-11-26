28c134354f7c5fcb135eb18b20f38821c978df57
Ulrik <ulrik.johansson@gmail.com>
2017-11-26 21:03:09 +0100

    Make builder role a state machine

--- role.builder.js
+++ role.builder.js
@@ -1,52 +1,78 @@
+const STATE_SPAWNING = 0;
+const STATE_MOVING_TO_ENERGY_STORE = 1;
+const STATE_FILLING_UP_ENERGY = 2;
+const STATE_MOVING_TO_CONSTRUCTION_SITE = 3;
+const STATE_CONSTRUCTING = 4;
+
+var state_lookup = function (state) {
+    let state_dict = {
+        0: "spawning",
+        1: "moving to energy store",
+        2: "filling up energy",
+        3: "moving to construction site",
+        4: "constructing"
+    }
+    return state_dict[state];
+}
+
 var roleBuilder = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
+        if(!creep.memory.state) {
+            creep.memory.state = STATE_SPAWNING;
+        }
 
-	    if(creep.memory.building && creep.carry.energy == 0) {
-            creep.memory.building = false;
-            creep.say('⚡ collect energy ');
+        switch(creep.memory.state) {
+            case STATE_SPAWNING:
+                this.runSpawning(creep);
+                break;
+            case STATE_MOVING_TO_ENERGY_STORE:
+                this.runMoveToEnergyStore(creep);
+                break;
+            case STATE_FILLING_UP_ENERGY:
+                this.runPickup(creep);
+                break;
+            case STATE_MOVING_TO_CONSTRUCTION_SITE:
+                this.runMoveToConstructionSite(creep);
+                break;
+            case STATE_CONSTRUCTING:
+                this.runConstruct(creep);
+                break;
+            default:
+                creep.memory.state = STATE_SPAWNING;
+                break;
+        }
+	},
+	runSpawning: function (creep) {
+	    if(!creep.spawning) {
+	        creep.memory.state = STATE_MOVING_TO_ENERGY_STORE;
+            creep.debug("new state {" + state_lookup(creep.memory.state) + "}");
 	    }
-	    if(!creep.memory.building && creep.carry.energy == creep.carryCapacity) {
-	        creep.memory.building = true;
-	        creep.say('build');
-	    }
-
-	    if(creep.memory.building) {
-	        var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
-
-	        //fix the smallest projects first
-	        targets = _.sortBy(targets, 'progressTotal');
-            if(targets.length) {
-                if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
-                }
+	},
+	runMoveToEnergyStore: function(creep) {
+        let target = creep.getClosestNonEmptyEnergyStore();
+        if (target) {
+            let results = creep.withdraw(target, RESOURCE_ENERGY);
+            if (results == ERR_NOT_IN_RANGE) {
+                creep.moveTo(target, {visualizePathStyle: {stroke: '#ffaa00'}});
             }
-	    }
-	    else {
-	        var targets = creep.room.find(FIND_STRUCTURES, {
-                    filter: (structure) => {
-                        return (structure.structureType == STRUCTURE_CONTAINER || structure.structureType == STRUCTURE_STORAGE) && structure.store[RESOURCE_ENERGY] > 0;
-                    }
-            });
-            if (targets.length > 0) {
-                var results = creep.withdraw(targets[targets.length -1], RESOURCE_ENERGY);
-                if (results == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[targets.length -1], {visualizePathStyle: {stroke: '#ffaa00'}});
+        } else {
+    	    target = creep.pos.findClosestByRange(FIND_DROPPED_ENERGY);
+            if(target) {
+                if(creep.pos.inRangeTo(target, 1)) {
+                    creep.memory.state = STATE_FILLING_UP_ENERGY;
+                    creep.debug("new state {" + state_lookup(creep.memory.state) + "}");
+                    this.runPickup(creep, target);
+                    return;
                 }
-            } else {
-        	    const target = creep.pos.findClosestByRange(FIND_DROPPED_ENERGY);
-                if(target) {
-                    if(creep.pos.inRangeTo(target, 1)) {
-                        this.runPickup(creep, target);
-                        return;
-                    }
-                    creep.moveTo(target, {visualizePathStyle: {stroke: '#ffaa00'}});
-
-                }
-
+                creep.moveTo(target, {visualizePathStyle: {stroke: '#ffaa00'}});
             }
-	    }
+        }
+        if (creep.carry.energy == creep.carryCapacity) {
+            creep.memory.state = STATE_MOVING_TO_CONSTRUCTION_SITE;
+            creep.debug("new state {" + state_lookup(creep.memory.state) + "}");
+        }
 	},
 	runPickup: function(creep, target) {
 	    if (!target) {
@@ -58,6 +84,53 @@ var roleBuilder = {
         }
 
         let result = creep.pickup(target);
+
+        if(creep.carry.energy == creep.carryCapacity) {
+            creep.memory.state = STATE_MOVING_TO_CONSTRUCTION_SITE;
+            creep.debug("new state {" + state_lookup(creep.memory.state) + "}");
+        }
+	},
+	runMoveToConstructionSite: function(creep) {
+	    let target = null;
+	    if(creep.memory.target_id) {
+	        target = Game.getObjectById(creep.memory.target_id);
+	    } else {
+	        let target = creep.findSmallestConstructionSite();
+            if (target) {
+                creep.memory.target_id = target.id;
+                creep.debug("Found new construction site: " + target + " @ " + target.pos);
+            } else {
+                creep.info("No construction sites found. Waiting...");
+                return;
+            }
+	    }
+
+        if(target) {
+            if(creep.pos.inRangeTo(target, 1)) {
+                creep.memory.state = STATE_CONSTRUCTING;
+                creep.debug("new state {" + state_lookup(creep.memory.state) + "}");
+                this.runConstruct(creep);
+                return;
+            } else {
+                creep.debug("Moving towards " + target + " @ " + target.pos);
+                creep.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}});
+            }
+        }
+	},
+	runConstruct: function(creep) {
+	    let target = Game.getObjectById(creep.memory.target_id);
+        let result = creep.build(target);
+        creep.debug("Build result: " + result);
+        if (result == ERR_INVALID_TARGET) {
+            creep.debug("Invalid target, deleting from memory: " + target);
+            delete creep.memory.target_id;
+            creep.memory.state = STATE_MOVING_TO_CONSTRUCTION_SITE;
+        }
+        if (result == ERR_NOT_ENOUGH_RESOURCES) {
+            creep.debug("Out of energy, going to get more. Resetting target as well");
+            delete creep.memory.target_id;
+            creep.memory.state = STATE_MOVING_TO_ENERGY_STORE;
+        }
 	}
 };
 
